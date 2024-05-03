fca1f593903ecb69f69ac8f9aee14618d2f7367d
Habbo Chabbo <habbo2001chabbo@gmail.com>
2024-05-03 21:52:53 +0100

    split the project into headers to smplify the project

--- src/header/Register.h
+++ src/header/Register.h
@@ -10,27 +10,3 @@ void clearREG(REG_t* reg){
   reg->content = 0;
 }
 
-typedef struct Counter {
-  REG_t reg;
-} COUNT_t;
-COUNT_t newCOUT(){
-  COUNT_t cout;
-  cout.reg.content = 0;
-  return cout;
-}
-void loadCOUT(COUNT_t* count, short value){
-  count->reg.content = value;
-}
-void incrCount(COUNT_t* count){
-  short c = count->reg.content;
-  c += 0b00000001;
-  count->reg.content = c;
-}
-void decrCount(COUNT_t* count){
-  short c = count->reg.content;
-  c -= 0b00000001;
-  count->reg.content = c;
-}
-short outCount(COUNT_t* count){
-  return count->reg.content;
-}
