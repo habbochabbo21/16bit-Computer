6ccc964e44e99b2419a4a8317844bf3d6157b695
Habbo Chabbo <84484768+habbochabbo21@users.noreply.github.com>
2024-05-03 21:02:01 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -3,6 +3,23 @@
 #include <string.h>
 
 struct Register {
-  char content;
+  short content;
 } REG_t;
-REG_t 
+REG_t newREG(){
+  REG_t reg;
+  return reg;
+}
+void clearREG(REG_t* reg){
+  reg->content = 0;
+}
+
+struct Counter {
+  REG_t reg;
+  char der;
+} COUT_t;
+COUT_t newCOUT(){
+  COUT_t cout;
+  cout.reg = 0;
+  cout.der = 0;
+  return cout;
+}
